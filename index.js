b4b7c01f479bd2e1772508c1a67f1b3c7575de78
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-05-20 15:15:45 +0600

    deleted function done

--- index.js
+++ index.js
@@ -66,6 +66,13 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/myToy/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id: new ObjectId(id)};
+        const result= await toyCollection.deleteOne(query);
+        res.send(result);
+    })
+
 
 
 
