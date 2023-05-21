e36f284682475d44076c21fc787e79e653be8898
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-05-21 12:11:22 +0600

    vercel depdoy done

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await 
+    client.connect();
 
 
     const toyCollection=client.db('sportsCarToy').collection('toys');
